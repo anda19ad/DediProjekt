1c7295d010ab7a7d70bc2b829eca45fae63310f6
ahellow <42740361+Hellowitos@users.noreply.github.com>
2021-11-05 16:52:44 +0100

    kommentarer tilføjet

--- views/Home.js
+++ views/Home.js
@@ -10,86 +10,99 @@ import Slider from '@react-native-community/slider';
 
 var DemoUsers = Demo;
 
+//her defineres home (main page) som eksporteres til sidst i filen.
+
 const Home = () => {
   const swiper = useRef(null)
 
-const slidingHandler = (value) => {
-  console.log(value)
-  if (value >= 5) {
-    console.log('if')
-    console.log(swiper.current)
-    swiper.current.swipeRight()
-    DemoUsers.splice();
-  } else {
-    console.log('else')
-    swiper.current.swipeLeft()
+  //sliding handler er lavet for at tjekke hvad brugeren vurderer de andre brugere til.
+  // if statement tjekker om bruger vurderer over eller under 5, som er accept grænsen for brugeren.
+  const slidingHandler = (value) => {
+    console.log(value)
+    if (value >= 5) {
+      console.log('if')
+      console.log(swiper.current)
+      swiper.current.swipeRight()
+      DemoUsers.splice();
+    } else {
+      console.log('else')
+      swiper.current.swipeLeft()
+    }
+
   }
 
-}
-
   return (
-    <ImageBackground
-      source={require('../images/bg.png')}
-      style={styles.bg}
-    >
-      <View style={styles.containerHome}>
-        <View style={styles.top}>
-          <City />
-          <Filters />
+      <ImageBackground
+          source={require('../images/bg.png')}
+          style={styles.bg}
+      >
+        <View style={styles.containerHome}>
+          <View style={styles.top}>
+            <City />
+            <Filters />
+          </View>
+
+          /* Nedenfor defineres  'reglerne' for cardstacken så de kontinuerligt bliver loopet, swiper korrekt retning og lign*/
+
+          <CardStack
+              loop={true}
+              verticalSwipe={false}
+              renderNoMoreCards={() => null}
+              ref={swiper}
+          >
+            /* Demo er arrayet af alle demo bruger objekter som med .map smækker dem sammen så de hentes i rækkefølge og sådan */
+
+            {Demo.map((item, index) => (
+                <Card key={index}>
+                  {/* Nedenfor laves profil kortet med relevante data og funktionalitet*/}
+                  <CardItem
+                      image={item.image}
+                      name={item.name}
+                      description={item.description}
+                      matches={item.match}
+                      action
+
+                      //nedenstående logik var til så brugeren kunne trykke på like/dislike knap i første udkast af MVP. Bruges ikke længere, men så flot lavet at det ikke skal slettes :)
+
+                      onPressLeft={() => {
+                        console.log(swiper),
+                            swiper.current.props.onSwipedLeft()}
+                      }
+                      onPressRight={() => swiper.current.props.onSwipedRight()}
+
+                  />
+
+                </Card>
+            ))}
+
+            /* Slideren laves her, fordi den skal ligge inde i samme <view> for at virke ordentligt. Hvis den fjernes herfra vil den ikke kende næste objekt der skal vises.*/
+
+          </CardStack>
+          {/* Nedenfor laves slideren baseret på react-native-community library*/}
+          <Slider
+              style={{paddingTop: 1000}}
+              trackStyle={styles.track}
+              thumbStyle={styles.thumb}
+              minimumTrackTintColor='#d14ba6'
+              minimumValue={1}
+              maximumValue={10}
+              maximumTrackTintColor="#000000"
+              onSlidingComplete={(value) => {
+
+                //Kalder slidingHandler til at håndtere værdien i slideren, samt hvornår den skal swipe ja/nej
+
+                slidingHandler(value)
+
+                //this.swiper.current.props.slidingHandler()
+              }
+              }
+          />
         </View>
-
-        <CardStack
-          loop={true}
-          verticalSwipe={false}
-          renderNoMoreCards={() => null}
-          ref={swiper}
-        > 
-
-          {Demo.map((item, index) => (
-            <Card key={index}>
-              {/* Nedenfor laves profil kortet med relevante data og funktionalitet*/}
-              <CardItem 
-                image={item.image}
-                name={item.name}
-                description={item.description}
-                matches={item.match}
-                action
-                
-                onPressLeft={() => { 
-                  console.log(swiper),
-                  swiper.current.props.onSwipedLeft()}
-                } 
-                onPressRight={() => swiper.current.props.onSwipedRight()}
-              
-              />
-              
-            </Card>
-          ))}
-          
-        
-        </CardStack>
-       {/* Nedenfor laves slideren baseret på react-native-community library*/}
-        <Slider
-          style={{paddingTop: 1000}}
-          trackStyle={styles.track}
-          thumbStyle={styles.thumb}
-          minimumTrackTintColor='#d14ba6'
-          minimumValue={1}
-          maximumValue={10}
-          maximumTrackTintColor="#000000"
-          onSlidingComplete={(value) => {
-            slidingHandler(value)
-      
-            //this.swiper.current.props.slidingHandler()
-          }
-        }
-        />
-      </View>
-    </ImageBackground>
+      </ImageBackground>
   );
 };
 
-    //Opretter en slidingHandler til at håndtere værdien i slideren, samt hvornår den skal swipe ja/nej
-    
+//Opretter en slidingHandler til at håndtere værdien i slideren, samt hvornår den skal swipe ja/nej
+
 
 export default Home;
